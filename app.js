9a8e04c135e71240f88c4bfdbe82c3ef1791ea70
imran-94 <82095311+imran-94@users.noreply.github.com>
2021-07-12 11:32:44 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll', () => {
     var scroll_position =window.scrollY;
-    if (scroll_position > 100) {
-        header.style.backgroundColor = '#29323c'
+    if (scroll_position > 40) {
+        header.style.backgroundColor = '#08263c'
     }   else {
         header.style.backgroundColor = 'transparent'
     }
@@ -23,3 +23,60 @@ menu_item.forEach((item) => {
      mobile_menu.classList.toggle('active');
   });
 });
+
+
+
+
+    var slides = document.querySelectorAll('.slide');
+    var btns = document.querySelectorAll('.btn');
+    let currentSlide = 1;
+
+    // Javascript for image slider manual navigation
+    var manualNav = function(manual){
+      slides.forEach((slide) => {
+        slide.classList.remove('active');
+
+        btns.forEach((btn) => {
+          btn.classList.remove('active');
+        });
+      });
+
+      slides[manual].classList.add('active');
+      btns[manual].classList.add('active');
+    }
+
+    btns.forEach((btn, i) => {
+      btn.addEventListener("click", () => {
+        manualNav(i);
+        currentSlide = i;
+      });
+    });
+
+    // Javascript for image slider autoplay navigation
+    var repeat = function(activeClass){
+      let active = document.getElementsByClassName('active');
+      let i = 1;
+
+      var repeater = () => {
+        setTimeout(function(){
+          [...active].forEach((activeSlide) => {
+            activeSlide.classList.remove('active');
+          });
+
+        slides[i].classList.add('active');
+        btns[i].classList.add('active');
+        i++;
+
+        if(slides.length == i){
+          i = 0;
+        }
+        if(i >= slides.length){
+          return;
+        }
+        repeater();
+      }, 10000);
+      }
+      repeater();
+    }
+    repeat();
+    
